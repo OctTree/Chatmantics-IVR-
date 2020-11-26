f5614c8bbc310429b0c525b1226fc2381dedfb5d
oct-cybertree <devware090@gmail.com>
2020-11-26 15:31:42 -0600

    SJ#01

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.static('assets'));
 const TO_NUMBER = '+18333210371';
+// const TO_NUMBER_SJ = '+18334800563';
+const TO_NUMBER_SJ = '+16299999319';
 
 app.get('/webhooks/answer', (req, res) => {
     const ncco = [{
@@ -56,4 +58,48 @@ app.post('/webhooks/dtmf', (req, res) => {
 
 })
 
+app.get('webhooks_sj/answer', (req, res) => {
+    const ncco = [{
+        action: "stream",
+        streamUrl: ["http://3.88.217.29:3000/stream/Credit_Repair_IVR.mp3"],
+        level: 1,
+        bargeIn: true
+
+    },
+    {
+        action: 'input',
+        maxDigits: 1,
+        eventUrl: [`${req.protocol}://${req.get('host')}/webhooks_sj/dtmf`]
+    }
+    ]
+
+    res.json(ncco)
+
+})
+
+app.post('webhooks_sj/dtmf', (req, res) => {
+    if (req.body.dtmf == '1') {
+        const ncco = [{
+            action: 'talk',
+            text: `Please hold while we transfer your call.`
+        },
+        {
+            action: 'connect',
+            from: req.body.from,
+            endpoint: [{
+                type: 'phone',
+                number: TO_NUMBER_SJ
+            }]
+        },
+        ]
+        res.json(ncco)
+    }
+    else if (req.body.dtmf == '2') {
+        const ncco = [{
+            action: 'talk',
+        }]
+        res.json(ncco)
+    }
+
+})
 app.listen(3000, () => console.log('listening...'))
