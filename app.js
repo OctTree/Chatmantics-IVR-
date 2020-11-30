ada2cb0e79ea8ee1ed8be085ea065bd0da515766
oct-cybertree <devware090@gmail.com>
2020-11-30 17:04:31 -0600

    Roll_back#01

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 require('dotenv').config()
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-
+const app = express();
 const creditrRouter = require('./routes/creditR');
 const creditr_sjRouter = require('./routes/creditR_SJ')
 const health_obRouter = require('./routes/health_OB')
 const port = process.env.PORT;
-const app = express();
-
-app.use(bodyParser.json());
-app.use(morgan('combined'));
 
 app.use('/webhooks', creditrRouter);
 app.use('/webhook_sj', creditr_sjRouter);
--- routes/creditR_SJ.js
+++ routes/creditR_SJ.js
@@ -1,62 +1,59 @@
+require('dotenv').config()
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
 // const TO_NUMBER_SJ = '+18334800563';
 const TO_NUMBER_SJ = process.env.TO_NUMBER_SJ;
 
+router.use(bodyParser.json());
 router.get('/answer', (req, res) => {
-    const ncco = [
-        {
-            action: "stream",
-            streamUrl: ["http://3.88.217.29:3000/stream/Credit_Repair_IVR.mp3"],
-            level: 1,
-            bargeIn: true
+    const ncco = [{
+        action: "stream",
+        streamUrl: ["http://3.88.217.29:3000/stream/Credit_Repair_IVR.mp3"],
+        level: 1,
+        bargeIn: true
 
-        },
-        {
-            action: 'input',
-            maxDigits: 1,
-            type: ['dtmf'],
-            dtmf: {
-                timeOut: 5,
-            },
-            eventUrl: [`${req.protocol}://${req.get('host')}/webhook_sj/dtmf`]
-        }
-    ];
+    },
+    {
+        action: 'input',
+        maxDigits: 1,
+        eventUrl: [`${req.protocol}://${req.get('host')}/webhook_sj/dtmf`]
+    }
+    ]
 
-    res.json(ncco);
+    res.json(ncco)
 
-});
+})
 
 router.post('/events', (req, res) => {
-    console.log(req.body);
+    console.log(req.body)
     res.send(200);
-});
+})
 
 router.post('/dtmf', (req, res) => {
     if (req.body.dtmf == '1') {
-        const ncco = [
-            {
-                action: 'talk',
-                text: `Please hold while we transfer your call.`
-            },
-            {
-                action: 'connect',
-                from: req.body.from,
-                endpoint: [{
-                    type: 'phone',
-                    number: TO_NUMBER_SJ
-                }]
-            },
-        ];
-        res.json(ncco);
+        const ncco = [{
+            action: 'talk',
+            text: `Please hold while we transfer your call.`
+        },
+        {
+            action: 'connect',
+            from: req.body.from,
+            endpoint: [{
+                type: 'phone',
+                number: TO_NUMBER_SJ
+            }]
+        },
+        ]
+        res.json(ncco)
     }
     else if (req.body.dtmf == '2') {
         const ncco = [{
             action: 'talk',
-        }];
-        res.json(ncco);
+        }]
+        res.json(ncco)
     }
 
-});
+})
 
 module.exports = router;
