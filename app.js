f37562c384ea71037cb9461dcaf6a805fb312f41
oct-cybertree <devware090@gmail.com>
2020-12-16 12:13:44 -0600

    Add_new_ivr#01

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const creditrRouter = require('./routes/creditR');
-const creditr_sjRouter = require('./routes/creditR_SJ')
-const health_obRouter = require('./routes/health_OB')
+const creditr_sjRouter = require('./routes/creditR_SJ');
+const health_obRouter = require('./routes/health_OB');
+const creditr_secRouter = require('./routes/creditR_Second');
 const port = process.env.PORT;
 
 app.use('/webhooks', creditrRouter);
 app.use('/webhook_sj', creditr_sjRouter);
 app.use('/webhook_ob', health_obRouter);
+app.use('/webhook_sec', creditr_secRouter);
 app.use(express.static('assets'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
