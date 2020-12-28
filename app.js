001e2ef4f0930dae84abcc0599f2fd2bcc510eed
devwre <devware090@gmail.com>
2020-12-28 17:05:14 -0600

    Add_IVR_TTS#01

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const creditrRouter = require('./routes/creditR');
 const creditr_sjRouter = require('./routes/creditR_SJ');
 const health_obRouter = require('./routes/health_OB');
 const creditr_secRouter = require('./routes/creditR_Second');
+const creditr_ttsRouter = require('./routes/creditR_TTS');
 const port = process.env.PORT;
 
 app.use('/webhooks', creditrRouter);
 app.use('/webhook_sj', creditr_sjRouter);
 app.use('/webhook_ob', health_obRouter);
 app.use('/webhook_sec', creditr_secRouter);
+app.use('/webhook_tts', creditr_ttsRouter);
 app.use(express.static('assets'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
