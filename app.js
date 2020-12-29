3545af880b5630b78aeae16778f752563787c360
devwre <devware090@gmail.com>
2020-12-29 10:10:33 -0600

    Add_IVR_AUTO#01

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const creditr_sjRouter = require('./routes/creditR_SJ');
 const health_obRouter = require('./routes/health_OB');
 const creditr_secRouter = require('./routes/creditR_Second');
 const creditr_ttsRouter = require('./routes/creditR_TTS');
+const dms_autoRouter = require('./routes/DMS_AUTO');
 const port = process.env.PORT;
 
 app.use('/webhooks', creditrRouter);
@@ -14,6 +15,7 @@ app.use('/webhook_sj', creditr_sjRouter);
 app.use('/webhook_ob', health_obRouter);
 app.use('/webhook_sec', creditr_secRouter);
 app.use('/webhook_tts', creditr_ttsRouter);
+app.use('/webhook_dms_auto', dms_autoRouter);
 app.use(express.static('assets'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
---
+++ routes/DMS_AUTO.js
@@ -0,0 +1,77 @@
+require('dotenv').config()
+const express = require("express");
+const router = express.Router();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const TO_DMS_AUTO = process.env.TO_DMS_AUTO;
+
+router.use(bodyParser.json());
+router.get('/answer', (req, res) => {
+
+    axios.get(`https://api.console.chatmantics.com/v1/dnc/check?phoneNumber=${req.query.from}`)
+        .then(response => {
+            console.log(`Chatmantics Internal CreditR => dnc: ${response.data.dnc}`);
+            if (response.data.dnc === false) {
+                const ncco = [{
+                    action: "stream",
+                    streamUrl: ["http://ivrn.chatmantics.com:3000/stream/Credit_Repair_IVR.mp3"],
+                    level: 1,
+                    bargeIn: true
+                },
+                {
+                    action: 'input',
+                    maxDigits: 1,
+                    eventUrl: [`${req.protocol}://${req.get('host')}/webhook_dms_auto/dtmf`]
+                }
+                ]
+
+                res.json(ncco);
+            }
+            else if(response.data.dnc === true){
+                const ncco = [{
+                    action: 'talk'
+                }]
+                res.json(ncco);
+            }
+        })
+        .catch(error => console.error(error));
+
+})
+
+router.post('/events', (req, res) => {
+    console.log(req.body)
+    res.send(200);
+})
+
+router.post('/dtmf', (req, res) => {
+    if (req.body.dtmf == '1') {
+        const ncco = [{
+            action: 'talk',
+            text: `Please hold while we transfer your call.`
+        },
+        {
+            action: 'connect',
+            from: req.body.from,
+            endpoint: [{
+                type: 'phone',
+                number: TO_DMS_AUTO
+            }]
+        },
+        ]
+        res.json(ncco)
+    }
+    else if (req.body.dtmf == '2') {
+        const ncco = [{
+            action: 'talk',
+        }]
+        try {
+            axios.get(`https://api.console.chatmantics.com/v1/dnc?phoneNumber=${req.body.from}`);
+        } catch (err) {
+            console.log(err);
+        }
+        res.json(ncco);
+    }
+
+})
+
+module.exports = router;
