bdaf628c79b589f971218dd9c4d2cdcb57213eb0
devwre <devware090@gmail.com>
2021-02-11 16:53:45 -0600

    outbound#04

--- routes/outBound.js
+++ routes/outBound.js
@@ -3,10 +3,27 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const Call_API_SID_Token = 'ACde8251b956b7958116ec3c1b59d41790';
+const varToken = 'f8ca067f-6742-48ef-8868-f325a5373d8d';
 
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
+    axios({
+        method: 'post',
+        url:  `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
+        data: {
+            From: '19174269335',
+            To: '19174269339'
+        },
+        headers: {
+          Authorization: 'basis ' + varToken
+        }
+    })
+    .then(response => {
+        console.log("calling....")
+    })
+    .catch(error => console.error(error))
     console.log(req.body);
     res.send('Yahoo!');
 })
