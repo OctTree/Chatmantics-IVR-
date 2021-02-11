aca213b7b1419e346976f99274d1e49d5183cf67
devwre <devware090@gmail.com>
2021-02-11 17:30:46 -0600

    outbound#06

--- routes/outBound.js
+++ routes/outBound.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const Call_API_SID_Token = 'ACde8251b956b7958116ec3c1b59d41790';
-const varToken = 'f8ca067f-6742-48ef-8868-f325a5373d8d';
+const Token = 'f8ca067f-6742-48ef-8868-f325a5373d8d';
 
+const varToken = btoa(`${Token}:${Call_API_SID_Token}`);
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
@@ -14,10 +15,11 @@ router.get('/', (req, res) => {
         url:  `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
         data: {
             From: '19174269335',
-            To: '19174269339'
+            To: '19174269339',
+            Url: 'https://api.teleapi.net/rcml/fourscore.xml',
         },
         headers: {
-          Authorization: 'basis ' + varToken
+          Authorization: `Basic ${varToken}`,
         }
     })
     .then(response => {
