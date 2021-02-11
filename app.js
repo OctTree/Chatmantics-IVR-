6f00a6939a6e9e2506d8b6e336eb473c1238842e
devwre <devware090@gmail.com>
2021-02-11 15:43:26 -0600

    outbound#01

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const health_obRouter = require('./routes/health_OB');
 const creditr_secRouter = require('./routes/creditR_Second');
 const creditr_ttsRouter = require('./routes/creditR_TTS');
 const dms_autoRouter = require('./routes/DMS_AUTO');
+const router = require('./routes/creditR');
 const port = process.env.PORT;
 
 app.use('/webhooks', creditrRouter);
@@ -17,5 +18,10 @@ app.use('/webhook_sec', creditr_secRouter);
 app.use('/webhook_tts', creditr_ttsRouter);
 app.use('/webhook_dms_auto', dms_autoRouter);
 app.use(express.static('assets'));
+
+router.get('/calling', (req, res) =>{
+    console.log(req.body)
+    res.send("Hello world!");
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
