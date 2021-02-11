88a1a6b0a8eacd58bbca1e6bba1f7ea32d061e76
devwre <devware090@gmail.com>
2021-02-11 17:41:55 -0600

    outbound#08

--- routes/outBound.js
+++ routes/outBound.js
@@ -10,23 +10,24 @@ const varToken = btoa(`${Token}:${Call_API_SID_Token}`);
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-    axios({
-        method: 'post',
-        url:  `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
-        data: {
-            From: '19174269335',
-            To: '19174269339',
-            Url: 'https://api.teleapi.net/rcml/fourscore.xml',
-        },
-        headers: {
-          Authorization: `Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=`,
-        }
-    })
-    .then(response => {
-        console.log("calling....")
-    })
-    .catch(error => console.error(error))
+    // axios({
+    //     method: 'post',
+    //     url:  `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
+    //     data: {
+    //         From: '19174269335',
+    //         To: '19174269339',
+    //         Url: 'https://api.teleapi.net/rcml/fourscore.xml',
+    //     },
+    //     headers: {
+    //       Authorization: `Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=`,
+    //     }
+    // })
+    // .then(response => {
+    //     console.log(response.data);
+    // })
+    // .catch(error => console.error(error))
     console.log(req.body);
+    res.send('Yahoo!');
 })
 
 module.exports = router;
