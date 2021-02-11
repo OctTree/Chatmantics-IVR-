5e271e1ae20e48ba37ff3752cf68c011f11bd3fc
devwre <devware090@gmail.com>
2021-02-11 17:52:42 -0600

    outbound#11

--- routes/outBound.js
+++ routes/outBound.js
@@ -7,6 +7,23 @@ const axios = require('axios');
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
+    axios({
+        method: 'post',
+        url: `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
+        data: {
+            From: '19174269335',
+            To: '19174269339',
+            Url: 'https://api.teleapi.net/rcml/fourscore.xml',
+        },
+        headers: {
+            Authorization: `Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=`,
+        }
+    })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => console.error(error))
+    console.log(req.body);
     console.log(req.body);
     res.send('Yahoo!');
 })
