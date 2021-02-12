0ac37574e25a98c8eadd1c0ed36934a4caa56b83
devwre <devware090@gmail.com>
2021-02-11 18:15:35 -0600

    outbound#18

--- routes/outBound.js
+++ routes/outBound.js
@@ -11,11 +11,11 @@ router.get('/', async (req, res) => {
         let response = await axios({
             method: 'post',
             url: `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
-            data: {
+            data: `{
                 From: '19174269335',
                 To: '19174269339',
                 Url: 'https://api.teleapi.net/rcml/fourscore.xml',
-            },
+            }`,
             headers: {
                 Authorization: `Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=`,
             }
