aca583b83735532852fde3de657016d01eda52da
devwre <devware090@gmail.com>
2021-02-12 01:16:45 -0600

    outbound#24

--- routes/outBound.js
+++ routes/outBound.js
@@ -2,14 +2,16 @@ require('dotenv').config()
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 const axios = require('axios');
 var FormData = require('form-data');
+const buffer = require('buffer/').Buffer;
 const Call_API_SID_Token = 'ACde8251b956b7958116ec3c1b59d41790';
-const Token = 'f8ca067f-6742-48ef-8868-f325a5373d8d'
-router.use(bodyParser.json());
+const Token = 'f8ca067f-6742-48ef-8868-f325a5373d8d';
 
 router.get('/', (req, res) => {
     var data = new FormData();
+    var varToken = buffer.from(`${Token}:${Call_API_SID_Token}`).toString('base64');
     data.append('From', req.query.callerId);
     data.append('To', req.query.forwardTo);
     data.append('Url', 'https://api.teleapi.net/rcml/fourscore.xml');
@@ -18,7 +20,7 @@ router.get('/', (req, res) => {
         method: 'post',
         url: `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
         headers: {
-            'Authorization': 'Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=',
+            'Authorization': `Basic ${varToken}`,
             ...data.getHeaders()
         },
         data: data
