339d9cce50ff62cc29a7588c37e3fca70a22777e
devwre <devware090@gmail.com>
2021-02-11 19:00:34 -0600

    outbound#20

--- routes/outBound.js
+++ routes/outBound.js
@@ -3,27 +3,34 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
 const axios = require('axios');
+var FormData = require('form-data');
 const Call_API_SID_Token = 'ACde8251b956b7958116ec3c1b59d41790';
+const Token = 'f8ca067f-6742-48ef-8868-f325a5373d8d';
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-    axios({
+    var data = new FormData();
+    data.append('From', req.callerId);
+    data.append('To', req.forwardTo);
+    data.append('Url', 'https://api.teleapi.net/rcml/fourscore.xml');
+
+    var config = {
         method: 'post',
         url: `https://api.teleapi.net/call/2012-04-24/Accounts/${Call_API_SID_Token}/Calls.json`,
-        data: {
-            From: '19174269335',
-            To: '19174269339',
-            Url: 'https://api.teleapi.net/rcml/fourscore.xml',
-        },
         headers: {
-            Authorization: `Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=`,
-        }
-    }).then(response => {
-        console.log(response.data);
-        res.send('Yahoo!');
-    }).catch(e => {
-        console.error(e);
-    })
+            'Authorization': 'Basic ZjhjYTA2N2YtNjc0Mi00OGVmLTg4NjgtZjMyNWE1MzczZDhkOkFDZGU4MjUxYjk1NmI3OTU4MTE2ZWMzYzFiNTlkNDE3OTA=',
+            ...data.getHeaders()
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 })
 
 module.exports = router;
